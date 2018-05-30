74d4c1fd327cfda0aa865e1b01e5ae8720fbc90b
jiusong <jiusong@ebay.com>
2018-05-30 20:41:25 +0800

    udpated.

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,15 @@
 
 <template>
-  <div>home</div>
+  <home-header></home-header>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
