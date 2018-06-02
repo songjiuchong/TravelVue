58bdab6fe9d153050f1967b215d71b4cba42fc93
jiusong <jiusong@ebay.com>
2018-06-02 15:11:14 +0800

    updated.

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,8 +2,8 @@
 <template>
   <div class="wrapper">
     <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList">
-        <img class="swiper-img" :src="item.imgUrl" key="item.id"/>
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -20,11 +20,11 @@ export default {
         loop: true
       },
       swiperList: [{
-        id:'0001',
-        imgUrl:'https://imgs.qunarzz.com/piao/fusion/1805/d1/5e0bc05b6dfa5f02.jpg'
-      },{
-        id:'0002',
-        imgUrl:'https://img1.qunarzz.com/des-mis/ad/1706/ef/d591e9a0ac403d.jpg'
+        id: '0001',
+        imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1805/d1/5e0bc05b6dfa5f02.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://img1.qunarzz.com/des-mis/ad/1706/ef/d591e9a0ac403d.jpg'
       }]
     }
   }
